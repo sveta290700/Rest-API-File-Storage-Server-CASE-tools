c05a555ef67327c17c1735d1a6c8993a06d6a8aa
sveta290700 <777rsb@mail.ru>
2021-10-31 19:12:53 +1000

    Improvements with case tools

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * FileController.php
+ *
+ * PHP version 7.4
+ *
+ * @category Class
+ * @package  Controllers
+ * @author   Reva Svetlana <777rsb@mail.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 namespace App\Http\Controllers;
 
 use App\Models\FileModel as File;
@@ -7,15 +17,39 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
+/**
+ * FileController Class
+ *
+ * Class
+ *
+ * @category Class
+ * @package  Controllers
+ * @author   Reva Svetlana <777rsb@mail.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
 class FileController extends Controller
 {
+    /**
+     * Function for getting all files
+     *
+     * @return JsonResponse
+     */
     public function getAll(): JsonResponse
     {
         $files = File::all();
         return response()->json($files);
     }
 
+    /**
+     * Function for file uploading
+     *
+     * @param Request $request - request for file downloading
+     *
+     * @return JsonResponse
+     */
     public function upload(Request $request): JsonResponse
     {
         if ($file = $request->file('file')) {
@@ -23,48 +57,69 @@ class FileController extends Controller
             $size = $file->getSize();
             $path = $file->store('public/storage');
             $file = new File();
-            $file->title = $title;
-            $file->size = $size;
-            $file->path = $path;
+            $file->setTitle($title);
+            $file->setSize($size);
+            $file->setPath($path);
             $file->save();
-            return response()->json([
+            return response()->json(
+                [
                 "message" => "File successfully uploaded",
-            ], 201);
-        }
-        else
-            return response()->json([
+                ], 201
+            );
+        } else {
+            return response()->json(
+                [
                 "message" => "File was not found"
-            ], 404);
+                ], 404
+            );
+        }
     }
 
+    /**
+     * Function for file downloading
+     *
+     * @param string $title - file title
+     *
+     * @return StreamedResponse|JsonResponse
+     */
     public function download(string $title)
     {
         $file = File::where('title', '=', $title)->first();
-        if ($file == Null)
-        {
-            return response()->json([
+        if ($file == null) {
+            return response()->json(
+                [
                 "error" => "File was not found"
-            ], 404);
+                ], 404
+            );
+        } else {
+            return Storage::download($file->getPath(), $file->getTitle());
         }
-        else
-            return Storage::download($file->path, $file->title);
     }
 
+    /**
+     * Function for file deleting
+     *
+     * @param string $title - file title
+     *
+     * @return JsonResponse
+     */
     public function delete(string $title): JsonResponse
     {
         $file = File::where('title', '=', $title)->first();
-        if ($file == Null)
-        {
-            return response()->json([
+        if ($file == null) {
+            return response()->json(
+                [
                 "error" => "File was not found"
-            ], 404);
-        }
-        else {
-            Storage::delete($file->path);
+                ], 404
+            );
+        } else {
+            Storage::delete($file->getPath());
             $file->delete();
-            return response()->json([
+            return response()->json(
+                [
                 "message" => "File was deleted successfully"
-            ]);
+                ]
+            );
         }
     }
 }
--- app/Models/FileModel.php
+++ app/Models/FileModel.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * @mixin Builder
+ */
 class FileModel extends Model
 {
     use HasFactory;
@@ -12,4 +16,53 @@ class FileModel extends Model
     protected $fillable = [
         'title', 'size', 'path'
     ];
+
+    /**
+     * @return mixed|string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed|string $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return int|mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param int|mixed $size
+     */
+    public function setSize($size): void
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return false|mixed|string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param false|mixed|string $path
+     */
+    public function setPath($path): void
+    {
+        $this->path = $path;
+    }
+
 }
